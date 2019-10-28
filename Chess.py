5e13b70c59903c2a990d0651ddad3a8cc912a473
Sean Morgan <46581862+Firesean@users.noreply.github.com>
2019-10-28 16:44:25 -0700

    Modified the code for easier read on set_up_board and a function was created to simplify the design on the code.

--- Chess.py
+++ Chess.py
@@ -5,12 +5,13 @@ from Pieces import *
 
 
 class Chess:
+    default_colors = ["White", "Black"]
     default_board_size = 8
-    default_board_state = ([Pawn(Piece.piece_types[0])] * default_board_size) + \
+    default_board_state = ([Pawn(Piece.piece_types[0])] + \
                           [Rook(Piece.piece_types[3]), Knight(Piece.piece_types[1]),
                            Bishop(Piece.piece_types[2]), Queen(Piece.piece_types[4]),
                            King(Piece.piece_types[5]), Bishop(Piece.piece_types[2]),
-                           Knight(Piece.piece_types[1]), Rook(Piece.piece_types[3])]
+                           Knight(Piece.piece_types[1]), Rook(Piece.piece_types[3])])
 
     def __init__(self, board_size=default_board_size):
         # Declarations
@@ -29,16 +30,22 @@ class Chess:
             self.board[row] *= self.board_size
 
     def set_up_board(self):
-        for start, end, side, color in [[1, -1, -1, "White"], [self.board_size-2, self.board_size, 1, "Black"]]:
-            # Start row to End row by side then color is assigned
-            print(start, end, side, color)
-            for row in range(start, end, side):
-                for col in range(self.board_size):
-                    if row == start:
-                        self.board[row][col] = self.default_board_state[col]  # Sets pawns
-                    else:
-                        self.board[row][col] = self.default_board_state[col+self.board_size]  # Sets piece order
-                    self.board[row][col].set_color(color)
+        start, end, side, color = 1, -1, -1, self.default_colors[0]
+        self.set_pieces(start, end, side, color)
+
+        start, end, side, color = self.board_size-2, self.board_size, 1, self.default_colors[1]
+        self.set_pieces(start, end, side, color)
+
+    def set_pieces(self, start, end, side, color):
+        for row in range(start, end, side):
+            for col in range(self.board_size):
+                if row == start:
+                    piece = self.default_board_state[0].get_piece_type()
+                    self.board[row][col] = eval("{0}({1},{2})".format(piece, row, col))  # Sets pawns
+                else:
+                    piece = self.default_board_state[col+1].get_piece_type()
+                    self.board[row][col] = eval("{0}({1},{2})".format(piece, row, col))  # Sets piece order
+                self.board[row][col].set_color(color)
 
     def get_space(self, x, y):
         return self.board[x][y]
@@ -51,6 +58,7 @@ class Chess:
                     continue
                 print(Piece.pieces[piece.get_image()], end=" ")
             print("\n")
+
 
 Chess()
 
